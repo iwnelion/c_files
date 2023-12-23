4af1524e662e1fe51d2a2026ab28a74477a0f508
Iwn Elion <153987262+iwnelion@users.noreply.github.com>
2023-12-23 22:02:48 +0200

    Update scan_symbols.c

--- scan_symbols.c
+++ scan_symbols.c
@@ -3,9 +3,9 @@
 int main() {
 int x,y,sum;
 char shit;
-printf("gimme two numbers and a damn symbol \n");
-scanf("%d %d %c",&x,&y,&shit);
-switch (shit){
+printf("gimme two numbers and a symbol \n");
+scanf("%d %d %c",&x,&y,&s);
+switch (s){
   case '+':
     {
     sum=x+y;
@@ -32,7 +32,7 @@ switch (shit){
     }
   default:
     {
-  printf("wrong symbol dumbass");
+  printf("wrong symbol");
     }  
 }
     return 0;

8d3710f247c16227cdc8f2c1d3cae42c66270f44
Iwn Elion <153987262+iwnelion@users.noreply.github.com>
2023-12-26 18:32:39 +0200

    Create print 2D array.c

---
+++ print 2D array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+int nums[2][3] = { {10,11,12},{20,21,22} };
+
+  printf("%d\n",nums[0][0]);
+
+/*
+runs:
+10
+it's the first place of row 0 and column 0.
+ */
+    return 0;
+}

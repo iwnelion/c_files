e5638861c50cc902394ffd2934f1e4d2944acc8e
Iwn Elion <153987262+iwnelion@users.noreply.github.com>
2023-12-26 20:01:22 +0200

    Create scan string.c

---
+++ scan string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+char name[30];
+printf("enter your name: \n");
+scanf("%s",name);
+printf("hello %s",name);
+
+/*
+enter your name:
+(name)
+hello (name)
+*/
+    return 0;
+}

52424549a9ca5fe4a43d4a965d99e44089c2e68f
Iwn Elion <153987262+iwnelion@users.noreply.github.com>
2023-12-27 18:53:13 +0200

    Create funct return 2 param.c

---
+++ funct return 2 param.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int fname(int x,int y){
+    return x+y;
+}
+int main() {
+printf("the result is: %d",fname(5,3));
+    return 0;
+}
+/*
+the result is: 8
+this program returns the sum of a function w 2 parameters: x+y
+*/

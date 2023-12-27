ccf54859794c65dbed07458607b3dda5946b754b
Iwn Elion <153987262+iwnelion@users.noreply.github.com>
2023-12-27 21:07:44 +0200

    Create math.c

---
+++ math.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    
+printf("%f\n", sqrt(16)); //4.000000
+
+printf("%f\n", ceil(1.4)); //rounds up: 2.000000
+printf("%f\n", floor(1.4)); //rounds down: 1.000000
+
+printf("%f", pow(4,3)); //x^y: 64.000000
+
+return 0;
+}

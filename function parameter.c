ff31859abb6ca23ade80fe804d1c42cf8928680d
Iwn Elion <153987262+iwnelion@users.noreply.github.com>
2023-12-27 18:31:46 +0200

    Update function parameter.c

--- function parameter.c
+++ function parameter.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 void namefunction(char name[]){
-    printf("hello %s\n",name);
+    printf("hello %s\n",name); \\name is a parameter
 }
 int main()
 {
-    namefunction("Jo");
+    namefunction("Jo"); \\Jo is an argument
     return 0;
 }
 //hello Jo

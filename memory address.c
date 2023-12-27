0d2c98d79ff408ecc06e3961dc9f3eba4d894d1a
Iwn Elion <153987262+iwnelion@users.noreply.github.com>
2023-12-27 16:52:15 +0200

    Create memory address.c

---
+++ memory address.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+int age=22;
+printf("i'm %p years old");
+
+/*
+runs:
+i'm 0x7ffe5367e044 years old
+
+the number is in hexadecimal form (0x..) 
+and it changes every time you run it.
+*/
+    return 0;
+}

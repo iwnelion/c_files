215f3e8466bfad39eee87cb1b60ea979e7ae4950
Iwn Elion <153987262+iwnelion@users.noreply.github.com>
2024-01-03 18:45:14 +0200

    Create array and pointer.c

---
+++ array and pointer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+char vowels[] = {'A','E','I','O','U'};
+char *pvowels = vowels;
+int i;
+
+for (i=0;i<5;i++) {
+    printf("&vowels[%d]: %p, pvowels = %d: %p, vowels + %d: %p \n", i,&vowels[i], i,pvowels + i, i, vowels + i);
+}
+for (i=0;i<5;i++) {
+    printf("vowels[%d]: %c, *(pvowels + %d): %c, *(vowels +%d): %c\n", i, vowels[i],i, *(pvowels + i),i, *(vowels + i));
+}
+//&vowels[i] gives the memory location of the i"th" element of the array vowels.
+
+    return 0;
+}

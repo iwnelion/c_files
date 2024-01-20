16076b3c1538d1dad4d203bd12047251369f16ba
Iwn Elion <153987262+iwnelion@users.noreply.github.com>
2024-01-20 13:53:11 +0200

    Create string library.c

---
+++ string library.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+/*  string library::
+strcpy() to copy a string over another string
+strcat() to append a string to another string
+strcmp() to compare two strings for equality
+strncmp() to compare the first n characters of two strings
+strlen() to calculate the length of a string
+*/
+
+    return 0;
+}

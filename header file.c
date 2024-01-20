076eef8d6fef5b4d9e64b8e92d31af3956b6d729
Iwn Elion <153987262+iwnelion@users.noreply.github.com>
2024-01-20 15:02:31 +0200

    Create header file.c

---
+++ header file.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "myfile.h"
+//or
+#include "folder/myfile.h"
+//header file: it ends with .c instead of .header
+//instead of the implementations of your functions and the other parts of a program, 
+//it holds the declarations.
+
+int main()
+{
+//program that uses another program (myfile.c)
+    return 0;
+}
+//this file doesn't do anything. it's just the basic syntax for header files.
